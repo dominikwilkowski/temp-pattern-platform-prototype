d90df96e17e33d16e9470d7c03134c3fc8d2f95d
dominikwilkowski <dominikwilkowski@users.noreply.github.com>
2022-09-14 05:22:04 +0000

    deploy: c34633c4c8b40ebfb400162efcda7f2d13185922

---
+++ _next/static/ZeEfLt61s8QXo2xLZdrQ6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-fe6d1145e31c12e7.js"],"/_error":["static/chunks/pages/_error-5b7b555486245cb3.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
