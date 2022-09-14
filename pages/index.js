16c2e57e89c1d12497fe2a2d151e37a5be1a9848
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2022-09-14 19:11:28 +1000

    fixed new-pattern form action

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home({ data }) {
 	return (
 		<Layout>
 			<form
-				action="/new-pattern"
+				action={`${basePath}/new-pattern`}
 				method="POST"
 				css={{
 					display: 'grid',
